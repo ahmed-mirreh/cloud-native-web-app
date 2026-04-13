0785d07a9d30587e2e1e8d00a1aedcc156afb9b5
ahmed-mirreh <ahmed.mirreh54@gmail.com>
2026-04-13 19:38:03 +0100

    use project name in path

--- terraform/remote-state.tf
+++ terraform/remote-state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "nba-higher-lower-game-terraform-state"
-    key          = "project-1/terraform.tfstate"
+    key          = "cloud-native-web-app/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
