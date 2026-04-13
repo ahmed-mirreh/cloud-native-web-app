a8a4f70f85aaab8d7896888c4f354f155bd4e330
ahmed-mirreh <ahmed.mirreh54@gmail.com>
2026-04-13 10:31:36 +0100

    Initial commit

---
+++ terraform/modules/networking/outputs.tf
@@ -0,0 +1,24 @@
+# Networking Module Outputs
+
+# VPC
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+# Subnets
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_route_table_ids" {
+  description = "IDs of the private route tables"
+  value       = aws_route_table.private[*].id
+}
+
---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,32 @@
+# Networking Configuration
+
+# Project identification variables (passed from root)
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
